463c6a8600cf67afc596122a294ed459ceb3be48
SoFa99 <19cjyz@gmail.com>
2020-05-24 17:44:10 +0300

    3

--- test_loss_and_derivatives.py
+++ test_loss_and_derivatives.py
@@ -2,6 +2,6 @@ import numpy as np
 from loss_and_derivatives  import LossAndDerivatives
 
 def test_l2_reg_derivative():
-    x = np.array([1,1,2,2])
+    x = np.array([1,1,2,2,0])
     assert LossAndDerivatives.l2_reg(x) == 10
     
